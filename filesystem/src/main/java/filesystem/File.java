71863d67489e480b02df07101243607d1f4dfeba
dheerajspml <kr.dbharti98@gmail.com>
2026-09-05 23:03:56 +0900

    file system

---
+++ filesystem/src/main/java/filesystem/File.java
@@ -0,0 +1,12 @@
+package filesystem;
+
+public class File implements FileSystem {
+    String fileName;
+    public File(String fileName) {
+        this.fileName = fileName;
+    }
+    @Override
+    public void printContents() {
+        System.out.println("File name: " + fileName);
+    }
+}
